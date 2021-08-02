301fb4a1637e27c09554619b5b4d226b7408428e
GASArrow <81965088+GASArrow@users.noreply.github.com>
2021-08-02 17:25:02 +0300

    Create Task1_3.java

---
+++ Task1_3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task1_3 {
+    public static void main(String[] args) {
+        double x;
+        double y;
+        double result;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите x:");
+        x = scanner.nextDouble();
+        System.out.println("Введите y:");
+        y = scanner.nextDouble();
+
+        result = (Math.sin(x) + Math.cos(y))/(Math.cos(x) - Math.sin(y)) * Math.tan(x * y);
+
+        System.out.println(result);
+    }
+}
