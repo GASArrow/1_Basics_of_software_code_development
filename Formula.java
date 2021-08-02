a34e5ed35fcab3d70e4e064530d95df8c0678ebb
GASArrow <81965088+GASArrow@users.noreply.github.com>
2021-08-02 17:06:46 +0300

    Add files via upload

---
+++ Formula.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Formula {
+    public static void main(String[] args) {
+
+        double a;
+        double b;
+        double c;
+        double result;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите a:");
+        a = scanner.nextDouble();
+        System.out.println("Введите b:");
+        b = scanner.nextDouble();
+        System.out.println("Введите c:");
+        c = scanner.nextDouble();
+
+        result = (b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) / 2 * a - Math.pow(a, 3) * c + Math.pow(b, -2);
+
+        System.out.println(result);
+    }
+}
